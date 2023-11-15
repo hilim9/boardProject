0380013cc97205c0971a2a86c609be8019d36f8e
hilim9 <dlagydls1212@naver.com>
2023-11-15 18:24:12 +0900

    게시판 설정 중

---
+++ src/main/java/org/koreait/entities/BoardData.java
@@ -0,0 +1,41 @@
+package org.koreait.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data @Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class BoardData {
+
+    @Id @GeneratedValue
+    private Long seq;
+
+    @Column(length = 65, nullable = false)
+    private String poster;
+
+    @Column(length = 100, nullable = false)
+    private String subject;
+
+    @Lob
+    @Column(nullable = false)
+    private String content;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime regDt;
+
+    @UpdateTimestamp
+    @Column(insertable = false)
+    private LocalDateTime modDt;
+
+
+}

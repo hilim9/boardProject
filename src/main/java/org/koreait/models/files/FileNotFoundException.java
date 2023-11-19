9a9e1cb3b8c57a0b9a0b5866f0113d7ad219d833
hilim9 <dlagydls1212@naver.com>
2023-11-20 02:05:53 +0900

    파일 업로드 구현 중

--- src/main/java/org/koreait/models/files/FileNotFoundException.java
+++ src/main/java/org/koreait/models/files/FileNotFoundException.java
@@ -1,10 +1,11 @@
 package org.koreait.models.files;
 
 import org.koreait.commons.exceptions.CommonException;
+import org.springframework.http.HttpStatus;
 
 public class FileNotFoundException extends CommonException {
 
     public FileNotFoundException() {
-        super("파일을 찾을수 없습니다");
+        super("파일을 찾을수 없습니다", HttpStatus.BAD_REQUEST);
     }
 }
